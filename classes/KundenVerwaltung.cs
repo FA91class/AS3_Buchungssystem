ebd5ed233f92e1c45b63446412d481871dd5b3e4
j.pfalzgraf <j.pfalzgraf@ccvossel.de>
2020-11-30 10:26:42 +0100

    adding file changes from example (ugly, need rework!!!)

---
+++ classes/KundenVerwaltung.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AS3_Buchung
+{
+    class KundenVerwaltung
+    {
+        public List<Kunde> Kunden { get; } = new List<Kunde>();
+        public KundenVerwaltung()
+        {
+            var b1 = new Bestellung("B123456", new DateTime(2019, 10, 13), null) { Rechnung = new Rechnung { RechnungsNr = "R123456", RechnungsDatum = new DateTime(2019, 10, 14), RabattProzent = 0.02 } };
+            var bp1 = new BestellPosition(1, 4) { Artikel = new Artikel { EinzelPreis = 3.50, ArtikelNr = "9836", Bezeichnung = "Cappuccino" } };
+            b1.AddBestellPosition(bp1);
+            var bp2 = new BestellPosition(3, 1) { Artikel = new Artikel { EinzelPreis = 6.50, ArtikelNr = "1115", Bezeichnung = "Schokoliebe", Produkthinweis = "Schokoladen- und Vanilleeis mit Eierlikör, Schokoladensoße, hausgemachter Schlagsahne, Schokomandelsplittern und Cookies" } };
+            b1.AddBestellPosition(bp2);
+            var bp3 = new BestellPosition(2, 1) { Artikel = new Artikel { EinzelPreis = 3.00, ArtikelNr = "1548", Bezeichnung = "Pistazien-Eis", Produkthinweis = "Pistazieneis aus 100% sizilianischen Pistazien (aus Bronte)" } };
+            b1.AddBestellPosition(bp3);
+            var bp4 = new BestellPosition(5, 2) { Artikel = new Artikel { EinzelPreis = 3.80, ArtikelNr = "3759", Bezeichnung = "Prosecco", Produkthinweis = "1 Glas Cavalier Blanc de Blanc Brut" } };
+            b1.AddBestellPosition(bp4);
+            var bp5 = new BestellPosition(4, 2) { Artikel = new Artikel { EinzelPreis = 4.70, ArtikelNr = "3405", Bezeichnung = "Bruschetta", Produkthinweis = "6 Bruschetta mit hausgemachtem Pesto, Tomate, Zwiebel & frischem Parmesan überbacken" } };
+            b1.AddBestellPosition(bp5);
+
+            Kunden.Add(new Kunde(new List<Bestellung> { b1 })
+            {
+                KundenNr = "K123456",
+                Name = "Sandra Richter",
+                Strasse = "Bleibtreustraße 99",
+                PLZ = "12345",
+                Ort = "Berlin",
+            });
+        }
+}
