8cfd4f1bc8205cb44094e825575c70b0644f53ba
j.pfalzgraf <j.pfalzgraf@ccvossel.de>
2020-11-30 09:52:37 +0100

    implementing new models

---
+++ models/Kunde.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AS3_Buchung.models
+{
+    class Kunde
+    {
+        public int TischNr { get; set; }
+
+        public string KundenNr { get; set; }
+
+        public string Name { get; set; }
+
+        public string Ort { get; set; }
+
+        public string PLZ { get; set; }
+
+        public string Strasse { get; set; }
+
+        public Kunde(List<Bestellung> bestellungen)
+        {
+
+        }
+    }
+}
---
+++ models/Rechnung.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AS3_Buchung.models
+{
+    class Rechnung
+    {
+        private double sumNetto;
+
+        private double rabatProzent;
+
+        public string BerechnungsNr { get; set; }
+
+        public DateTime BerechnungsDatum { get; set; }
+
+        public double SummeNetto { get; set; }
+
+        public double RabatProzent { get; set; }
+
+        public double RabatPreis { get; set; }
+
+        public double NettoMitRabat { get; set; }
+
+        public double UstProzent { get; set; }
+
+        public double UstAufschlag { get; set; }
+
+        public double SummeBruto { get; set; }
+
+        public void Berechne()
+        {
+
+        }
+    }
+}
